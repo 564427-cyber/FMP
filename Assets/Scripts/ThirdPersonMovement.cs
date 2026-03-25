166a703025ba47416e91aa93912ded480d0f583e
564427-cyber <564427@escg.ac.uk>
2026-03-25 15:52:53 +0000

    end of 25/3/26

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -2,26 +2,56 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    //camera
+
     public CharacterController controller;
     public Transform cam;
-
     public float speed = 6f;
-
     public float turnSmoothTime = 0.1f;
     float smoothTurnVelocity;
     Vector3 direction;
-  
- 
+
+    //jump
+
+    float verticalVelocity;
+    float gravity = -9.81f;
+    float jumpForce = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
     void Update()
     {
+        
         DoMovement();
+        
     }
 
     void DoMovement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
         direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            verticalVelocity = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 move = Vector3.zero;
 
         if (direction.magnitude >= 0.1f)
         {
@@ -30,7 +60,13 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            move = moveDir.normalized * speed;
         }
-    }   
+
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+
 }
