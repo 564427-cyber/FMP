c9c40ffc02c34027819c0154803d4aab608283bd
564427-cyber <564427@escg.ac.uk>
2026-04-27 15:23:47 +0100

    PlayerChar

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -14,7 +14,7 @@ public class ThirdPersonMovement : MonoBehaviour
     //jump
 
     float verticalVelocity;
-    float gravity = -9.81f;
+    float gravity = -15f;
     float jumpForce = 1.7f;
 
     public Transform groundCheck;
@@ -23,16 +23,26 @@ public class ThirdPersonMovement : MonoBehaviour
 
     bool isGrounded;
 
+
+    //Animation
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {        
         DoMovement();        
+        DoAnimation();
     }
 
     void DoMovement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
         direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -64,6 +74,43 @@ public class ThirdPersonMovement : MonoBehaviour
         move.y = verticalVelocity;
 
         controller.Move(move * Time.deltaTime);
+    }
+
+    void DoAnimation()
+    {
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        bool jumpPressed = Input.GetKeyDown(KeyCode.Space) && isGrounded;
+        bool jumpReset = Input.GetKeyUp(KeyCode.Space);
+
+        bool isMoving = direction.magnitude > 0.1f;
+        bool isRunning = isMoving && runPressed;
+
+        if (isRunning) 
+        {
+            animator.SetBool("Run", true);
+            animator.SetBool("Walk", false);
+            
+        }
+        else if (isMoving)
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Walk", false);
+        }
+
+        if (jumpPressed)
+        {
+            Debug.Log("jumpTriggered");
+            animator.SetBool("Jump", true);
+        }
+        if (jumpReset)
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 
 
